90b90d04fff494b70ad220bd82ac48d43f3096c9
Harald Welte <laforge@osmocom.org>
2020-01-13 18:20:39 +0100

    dump: Add support for writing LAPD PCAP of E1 superchannel

--- projects/riscv_usb/sw/dump.c
+++ projects/riscv_usb/sw/dump.c
@@ -29,9 +29,10 @@
 #include <errno.h>
 
 #include <osmocom/core/utils.h>
-
-
-static uint8_t g_usb_endpoint = 0x81;
+#include <osmocom/core/logging.h>
+#include <osmocom/core/application.h>
+#include <osmocom/core/isdnhdlc.h>
+#include <osmocom/abis/lapd_pcap.h>
 
 #define E1_CHUNK_HDR_MAGIC	0xe115600d /* E1 is good */
 struct e1_chunk_hdr {
@@ -44,18 +45,81 @@ struct e1_chunk_hdr {
 	uint8_t ep;		/* USB endpoint */
 } __attribute__((packed));
 
+static struct osmo_isdnhdlc_vars g_hdlc[2]; /* one per direction */
+static int g_pcap_fd = -1;
+static struct msgb *g_pcap_msg;
 
+/* called for each HDLC payload frame */
+static void handle_payload(uint8_t ep, const uint8_t *data, int len)
+{
+	int dir;
+
+	switch (ep) {
+	case 0x81:
+		dir = OSMO_LAPD_PCAP_INPUT;
+		break;
+	case 0x82:
+		dir = OSMO_LAPD_PCAP_OUTPUT;
+		break;
+	default:
+		fprintf(stderr, "Unexpected USB EP 0x%02x\n", ep);
+		return;
+	}
+
+	if (g_pcap_fd >= 0) {
+		uint8_t *cur = msgb_put(g_pcap_msg, len);
+		memcpy(cur, data, len);
+		osmo_pcap_lapd_write(g_pcap_fd, dir, g_pcap_msg);
+		msgb_reset(g_pcap_msg);
+	} else
+		printf("OUT[%02x]: %s\n", ep, osmo_hexdump(data, len));
+}
+
+
+/* called for each USB transfer read from the file */
 static void handle_frame(const struct e1_chunk_hdr *hdr, const uint8_t *data)
 {
+	uint8_t nots0[1024];
+	unsigned int offs = 0;
+	struct osmo_isdnhdlc_vars *hdlc;
+
 	/* filter on the endpoint (direction) specified by the user */
-	if (hdr->ep != g_usb_endpoint)
+	switch (hdr->ep) {
+	case 0x81:
+		hdlc = &g_hdlc[0];
+		break;
+	case 0x82:
+		hdlc = &g_hdlc[1];
+		break;
+	default:
+		fprintf(stderr, "Unexpected USB EP 0x%02x\n", hdr->ep);
 		return;
+	}
 
 	if (hdr->len <= 4)
 		return;
 
-	for (int i = 4; i < hdr->len-4; i += 32)
-		printf("%s\n", osmo_hexdump(data+i, 32));
+	for (int i = 4; i < hdr->len-4; i += 32) {
+		//printf("\t%s\n", osmo_hexdump(data+i, 32));
+		memcpy(nots0+offs, data+i+1, 32-1);
+		offs += 31;
+	}
+
+	//printf("IN: %s\n", osmo_hexdump(nots0, offs));
+	uint8_t out[512];
+	int rc;
+	int rl;
+
+	int oi = 0;
+
+	while (oi < offs) {
+		rc = osmo_isdnhdlc_decode(hdlc, nots0+oi, offs-oi, &rl, out, sizeof(out));
+		if (rc < 0)
+			fprintf(stderr, "ERR in HDLC decode: %d\n", rc);
+		else if (rc > 0)
+			handle_payload(hdr->ep, out, rc);
+		oi += rl;
+	}
 }
 
 static int process_file(int fd)
@@ -99,10 +163,22 @@ static int open_file(const char *fname)
 	return open(fname, O_RDONLY);
 }
 
+
+static const struct log_info_cat log_categories[] = {
+};
+
+static const struct log_info log_info = {
+	.cat = log_categories,
+	.num_cat = ARRAY_SIZE(log_categories),
+};
+
 int main(int argc, char **argv)
 {
 	char *fname;
 	int rc;
+	int i;
+
+	osmo_init_logging2(NULL, &log_info);
 
 	if (argc < 2) {
 		fprintf(stderr, "You must specify the file name of the ICE40-E1 capture\n");
@@ -115,5 +191,18 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error opening %s: %s\n", fname, strerror(errno));
 		exit(1);
 	}
+
+	if (argc >= 3) {
+		g_pcap_fd = osmo_pcap_lapd_open(argv[2], 0640);
+		if (g_pcap_fd < 0) {
+			fprintf(stderr, "Unable to open PCAP output: %s\n", strerror(errno));
+			exit(1);
+		}
+		g_pcap_msg = msgb_alloc(4096, "pcap");
+	}
+
+	for (i = 0; i < ARRAY_SIZE(g_hdlc); i++)
+		osmo_isdnhdlc_rcv_init(&g_hdlc[i], OSMO_HDLC_F_BITREVERSE);
+
 	process_file(rc);
 }
